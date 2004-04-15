116c96bc1b3a00c86923e34ad2fc045dca3a9083
victorvarela <victorvarela>
2004-04-15 23:05:05 +0000

    keeps smbwebclient.php current revision (again)

--- php/header.php
+++ php/header.php
@@ -3,7 +3,7 @@
 # File:    smbwebclient.php
 # Author:  Victor M. Varela <vmvarela@nivel0.net>
 #
-# {[$Id: header.php,v 1.2 2004/04/15 23:03:38 victorvarela Exp $]}
+# {Id: smbwebclient.php,v 1.76 2004/04/12 11:53:44 vmvarela Exp }
 #
 # Description:
 #    This script is a web interface to Windows Networks.
